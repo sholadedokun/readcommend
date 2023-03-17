a5adab14afd81fa944317c40821b5d9cbf77653e
Olushola <sholadedokun@gmail.com>
2023-03-17 11:02:10 -0600

    Added basic config for db connection

---
+++ service/SearchEngine/src/database/Sequalize.js
@@ -0,0 +1,18 @@
+import { Sequelize } from "sequelize"
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const config = require ("./config.json")
+
+// by default sequelize parses Decimal number as JS 
+Sequelize.DataTypes.postgres.DECIMAL.parse = parseFloat;
+const nodeEnv = process.env.NODE_ENV || "development"
+const {database, username, password, host, port, dialect, dialectOptions} = config[nodeEnv]
+
+const sequelize = new Sequelize(database, username, password, {
+  host,
+  port,
+  dialect,
+  dialectOptions
+});
+export default sequelize
